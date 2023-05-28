ed18a428a9641fa4252e1058d45831c854f232bb
しまぶー <0120koto@gmail.com>
2023-05-28 09:51:42 +0900

    🎨

--- src/app/(app)/(search)/foo/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Search() {
-  return <div className="p-2">foo</div>;
-}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,15 @@ const { withTV } = require("tailwind-variants/transformer");
 
 /** @type {import('tailwindcss').Config} */
 const config = {
+  darkMode: "class",
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       strokeWidth: {
         1.5: "1.5px",
+      },
+      fontSize: {
+        "2xs": "0.625rem",
       },
     },
   },
